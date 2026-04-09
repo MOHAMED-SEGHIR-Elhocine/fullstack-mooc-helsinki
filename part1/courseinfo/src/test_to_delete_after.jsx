80b6af415258d57cb4418a5373f3bf29381f4cc9
MOHAMED-SEGHIR-Elhocine <mselhocine@gmail.com>
2026-04-09 18:21:30 +0100

    exercise 1.5

---
+++ part1/courseinfo/src/test_to_delete_after.jsx
@@ -0,0 +1,47 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.name} {props.exercises}</p>
+  )
+
+}
+ 
+const Content =(props) => {
+  return (
+    <Part part = {props.part1} />
+  )
+
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course = {course}/ >
+      <Content part1 = {part1} part2 = {part2}  part3 = {part3}/>
+      <Total  part1 = {part1} part2 = {part2}  part3 = {part3} / >
+
+    
+    </div>
+  )
+}
