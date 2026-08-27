ba3038ca3e31d1d4ee311807f1fe8779e0e13a13
Elhocine Mohamed-Seghir <mselhocine@gmail.com>
2026-08-27 18:06:09 +0100

    completed Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,70 +1,74 @@
-import { useState } from 'react'
-import Filter from './components/Filter'
-import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
+import { useEffect, useState } from "react";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [search, setSearch] = useState('')
-   const available = persons.filter(person=> person.name.toLowerCase().includes(search.toLowerCase()) )
+  const [persons, setPersons] = useState([]);
 
-  const handleNameChange = (event)=> {
-    setNewName(event.target.value)
+  useEffect(()=> {
+    axios.get('http://localhost:3001/persons').
+    then(response=> {
+      setPersons(response.data)
+    })
 
-  }
+  }, [])
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
+  const available = persons.filter((person) =>
+    person.name.toLowerCase().includes(search.toLowerCase()),
+  );
 
-  const handleNumberChange = (event)=> {
-    setNewNumber(event.target.value)
-  }
-  const handleSearching = (event)=> {
-    setSearch(event.target.value)
-  }
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+  const handleSearching = (event) => {
+    setSearch(event.target.value);
+  };
   const addPerson = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const personObject = {
-      name : newName,
-      number : newNumber
-    }
-    const check = persons.some(person => person.name.toLowerCase() === newName.toLowerCase())
-    //console.log(check) 
+      name: newName,
+      number: newNumber,
+    };
+    const check = persons.some(
+      (person) => person.name.toLowerCase() === newName.toLowerCase(),
+    );
+    //console.log(check)
     if (check === true) {
-      alert(`${newName} is already added to phonebook`)
-      return
-     
-      
+      alert(`${newName} is already added to phonebook`);
+      return;
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
-
-  }
+    setPersons(persons.concat(personObject));
+    setNewName("");
+    setNewNumber("");
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-         <div>
-
-        
-<Filter search = {search} handleSearching={handleSearching} />
-        </div>
+      <div>
+        <Filter search={search} handleSearching={handleSearching} />
+      </div>
       <h3>add a new</h3>
-      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
-      
-      
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+
       <h3>Numbers</h3>
       <Persons available={available} />
-      
     </div>
-  )
-}
+  );
+};
 
-export default App
-
-
+export default App;
